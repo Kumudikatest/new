2810af8d722847eda757d42cacc7497216eef72d
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-03-23 09:56:36 +0530

    test

---
+++ New.js
@@ -0,0 +1,18 @@
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = async (event) => {
+    debugger;
+    try {
+        let data = await cognito_idp.listUsers({
+            UserPoolId: "us-east-1_HdYJb7Znp",
+            Limit: 10
+        }).promise();
+        console.log(data);
+
+    } catch (err) {
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed" };
+};
